7413cf71b1c4e621a2641bf63c560b96fb2974de
Artem_Lakatun <alakatuni@mail.ru>
2024-01-16 19:25:40 +0300

    feat: made a js code for the preview

---
+++ js/index.js
@@ -0,0 +1,34 @@
+window.addEventListener('DOMContentLoaded', () => {
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tab-header__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tab-header__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target &&  target.classList.contains('tab-header__item')) {
+            tabs.forEach((item, i) => {
+                if (target === item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            })
+        }
+    });
+});
--- js/variables.js
+++ js/variables.js
@@ -1,2 +1,3 @@
-const tabs = document.querySelectorAll('.tabheader__item');
-const tabsContent = document.querySelectorAll('.tabcontent')
+const tabs = document.querySelectorAll('.tab-header__item'),
+    tabsContent = document.querySelectorAll('.tab-content'),
+    tabsParent = document.querySelector('.tab-header__items');
