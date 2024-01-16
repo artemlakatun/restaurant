d1755be644072be6bd20d9d2b3277a9cd069d7d8
Artem_Lakatun <alakatuni@mail.ru>
2024-01-16 22:27:10 +0300

    feat: made order cards with oop

---
+++ classes/MenuCard.js
@@ -0,0 +1,33 @@
+class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+        this.src = src;
+        this.alt = alt;
+        this.title = title;
+        this.descr = descr;
+        this.price = price;
+        this.parent = document.querySelector(parentSelector);
+        this.transfer = 3.20;
+        this.changeToBYN();
+    }
+
+    changeToBYN() {
+        this.price = (this.price * this.transfer).toFixed(2);
+    }
+
+    render() {
+        const element = document.createElement('div');
+        element.innerHTML = `
+            <div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> рублей/день</div>
+                </div>
+            </div>
+        `;
+        this.parent.append(element);
+    }
+}
