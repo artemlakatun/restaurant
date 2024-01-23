a72717d7ed460b41df9f54a06db46ae03548ad0d
Artem_Lakatun <alakatuni@mail.ru>
2024-01-24 01:51:21 +0300

    feat: project is assembled with the help of webpack

---
+++ js/modules/tabs.js
@@ -0,0 +1,42 @@
+function tabs() {
+    // Tabs
+
+    let tabs = document.querySelectorAll('.tab-header__item'),
+        tabsContent = document.querySelectorAll('.tab-content'),
+        tabsParent = document.querySelector('.tab-header__items');
+
+    function hideTabContent() {
+
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tab-header__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tab-header__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', function(event) {
+        const target = event.target;
+        if(target && target.classList.contains('tab-header__item')) {
+            tabs.forEach((item, i) => {
+                if (target === item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+}
+
+export default tabs;
